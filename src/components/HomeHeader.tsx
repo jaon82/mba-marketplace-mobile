88a2cc4910ebb907de8b756b5666dc0aae2a87a1
Jean Nunes <jean.nunes@uzzipay.com>
2025-05-16 18:39:18 -0400

    Load user name and avatar

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,23 +1,31 @@
-import { Heading, HStack, Icon, Text, VStack } from '@gluestack-ui/themed'
-import { UserPhoto } from './UserPhoto'
-import { LogOut } from 'lucide-react-native'
+import defaulUserPhotoImg from "@assets/userPhotoDefault.png";
+import { Heading, HStack, Icon, Text, VStack } from "@gluestack-ui/themed";
+import useAuth from "@hooks/useAuth";
+import { LogOut } from "lucide-react-native";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
+  const { user } = useAuth();
+
   return (
-    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap='$4'>
-      <UserPhoto source={{ uri: 'https://github.com/arthurrios.png'}} w='$16' h='$16' alt='Imagem do usuário' />
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+      <UserPhoto
+        source={user.avatar ? { uri: user.avatar } : defaulUserPhotoImg}
+        w="$16"
+        h="$16"
+        alt="Imagem do usuário"
+      />
 
       <VStack flex={1}>
         <Text color="$gray100" fontSize="$sm">
           Olá
         </Text>
         <Heading color="$gray100" fontSize="$md">
-          Arthur Rios
+          {user.name}
         </Heading>
       </VStack>
 
-      <Icon as={LogOut} color='$gray200' size='xl' />
-
+      <Icon as={LogOut} color="$gray200" size="xl" />
     </HStack>
-  )
+  );
 }
