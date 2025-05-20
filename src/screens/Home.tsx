4c1e2b5cbad403a6cc2d98b0d14271fd3cc6563e
Jean Nunes <jean.nunes@uzzipay.com>
2025-05-20 19:33:46 -0400

    Fetch groups

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,27 +1,57 @@
-import { ExerciseCard } from '@components/ExerciseCard'
-import { Group } from '@components/Group'
-import { HomeHeader } from '@components/HomeHeader'
-import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
-import { useNavigation } from '@react-navigation/native'
-import { AppNavigatorRoutesProps } from '@routes/app.routes'
-import { useState } from 'react'
-import { FlatList } from 'react-native'
+import { ExerciseCard } from "@components/ExerciseCard";
+import { Group } from "@components/Group";
+import { HomeHeader } from "@components/HomeHeader";
+import { ToastMessage } from "@components/ToastMessage";
+import { Heading, HStack, Text, useToast, VStack } from "@gluestack-ui/themed";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+import api from "@services/api";
+import { AppError } from "@utils/AppError";
+import { useEffect, useState } from "react";
+import { FlatList } from "react-native";
 
 export function Home() {
+  const toast = useToast();
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
   const [exercises, setExercises] = useState([
-    'Puxada frontal',
-    'Remada curvada',
-    'Remada unilateral',
-    'Levantamento terra',
-  ])
-  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
-  const [groupSelected, setGroupSelected] = useState('costas')
-
-  const navigation = useNavigation<AppNavigatorRoutesProps>()
+    "Puxada frontal",
+    "Remada curvada",
+    "Remada unilateral",
+    "Levantamento terra",
+  ]);
+  const [groups, setGroups] = useState<string[]>([]);
+  const [groupSelected, setGroupSelected] = useState("costas");
 
   function handleOpenExerciseDetails() {
-    navigation.navigate('exercise')
+    navigation.navigate("exercise");
   }
+
+  async function fetchGroups() {
+    try {
+      const response = await api.get("/groups");
+      setGroups(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const errorTitle = isAppError
+        ? error.message
+        : "Não foi possível carregar os grupos musculares";
+      toast.show({
+        placement: "top",
+        render: ({ id }) => (
+          <ToastMessage
+            id={id}
+            action="error"
+            title={errorTitle}
+            onClose={() => toast.close(id)}
+          />
+        ),
+      });
+    }
+  }
+
+  useEffect(() => {
+    fetchGroups();
+  }, []);
 
   return (
     <VStack flex={1}>
@@ -64,5 +94,5 @@ export function Home() {
         />
       </VStack>
     </VStack>
-  )
+  );
 }
