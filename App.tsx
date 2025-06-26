b25218bd4eb4245a4b8a10bf7250385052b610ef
Jean Nunes <jean.nunes@uzzipay.com>
2025-06-25 23:07:46 -0400

    Add fonts and splash screen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,23 @@
 /* eslint-disable camelcase */
 import { Loading } from "@components/Loading";
 import { AuthContextProvider } from "@contexts/AuthContext";
+import { DMSans_700Bold } from "@expo-google-fonts/dm-sans";
 import {
-  Roboto_400Regular,
-  Roboto_700Bold,
+  Poppins_400Regular,
+  Poppins_500Medium,
   useFonts,
-} from "@expo-google-fonts/roboto";
+} from "@expo-google-fonts/poppins";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { Routes } from "@routes/index";
 import { StatusBar } from "react-native";
 import { config } from "./config/gluestack-ui.config";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
+  const [fontsLoaded] = useFonts({
+    DMSans_700Bold,
+    Poppins_400Regular,
+    Poppins_500Medium,
+  });
 
   return (
     <GluestackUIProvider config={config}>
